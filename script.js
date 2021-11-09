ea232ecabeeb428eb1ca8e1275a52753cd88ee87
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-09 18:17:51 +0100

    Fine MILESTONE 2

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 for(let i = 1; i <= 100; i++){
+    
+    let div = document.createElement('div');
+    div.classList.add("base-box");
+    document.getElementById("container").appendChild(div);
+
     if(i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz" + ": " + i);
+        div.innerHTML = "FizzBuzz";
     }
     else if(i % 3 == 0){
-        console.log("Fizz" + ": " + i);
+        div.innerHTML = "Fizz";
     }
     else if(i % 5 == 0){
-        console.log("Buzz" + ": " + i);
+        div.innerHTML = "Buzz";
     }
     else{
-          console.log(i);
+        div.innerHTML = i;
     }
 }
