6dffa722a291593f6d2d0c84b845ba2318f7a980
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-09 18:21:22 +0100

    Fine MILESTONE 3 // Fine Esercizio

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 for(let i = 1; i <= 100; i++){
-    
+
     let div = document.createElement('div');
     div.classList.add("base-box");
     document.getElementById("container").appendChild(div);
 
     if(i % 3 == 0 && i % 5 == 0){
         div.innerHTML = "FizzBuzz";
+        div.classList.add("FizzBuzz");
     }
     else if(i % 3 == 0){
         div.innerHTML = "Fizz";
+        div.classList.add("Fizz");
     }
     else if(i % 5 == 0){
         div.innerHTML = "Buzz";
+        div.classList.add("Buzz");
     }
     else{
         div.innerHTML = i;
